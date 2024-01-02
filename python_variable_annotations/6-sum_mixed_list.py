7ed54c34dfd21a6b9ed215682a81f7c69a90c280
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-02 16:10:26 +0100

    Changed type order

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sums a list of mixed values"""
     return sum(mxd_lst)
