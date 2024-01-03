d1ac7a94cb5f1abd47fd9a57556af41880e5d7d1
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-03 15:43:04 +0100

    Addend ensure future

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,4 +6,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay):
     """create a task"""
-    return asyncio.create_task(wait_random(max_delay))
+    return asyncio.ensure_future(wait_random(max_delay))
