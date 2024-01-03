e2dc339710160d3899360a10d834a77c97568b55
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-03 16:35:00 +0100

    try no default value

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """exec multiple routines at once"""
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
