ece44d6c486e0b684df2ddecacd70c49c8562f32
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-03 15:14:11 +0100

    Try other method

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,13 +5,13 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
+async def wait_append(ret_list, max_delay):
+    ret_list.append(await wait_random(max_delay))
+
+
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """exec multiple routines at once"""
+    """sleep between 0 and max"""
     delays = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
-
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-
+    tasks = [wait_append(delays, max_delay) for _ in range(n)]
+    await asyncio.gather(*tasks)
     return delays
