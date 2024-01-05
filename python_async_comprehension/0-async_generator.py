c32832b1be1ce02700747ae337c5025222b647f2
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-05 16:54:38 +0100

    Changed async generator to generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """generate 10 numbers"""
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     this function
     generates 10 numbers
