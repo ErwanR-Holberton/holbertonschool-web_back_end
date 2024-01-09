f9c55a64e9e2a0ee4c6576d2c6168845820b924b
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-09 19:16:58 +0100

    added comment

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """is supposed to work when some data is deleted ?"""
         assert (
             index is None or (isinstance(index, int)
                               and 0 <= index < len(self.dataset()))
