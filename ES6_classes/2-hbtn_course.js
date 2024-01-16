5ecc2503fed198d5a5f66aa05a80f973496a9d02
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-16 13:33:41 +0100

    removed extra functions

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,49 +1,38 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (!this.validateString(name)) { throw new TypeError('Name must be a string'); }
-    if (!this.validateNumber(length)) { throw new TypeError('Length must be a number'); }
-    if (!this.validateStudents(students)) { throw new TypeError('Students must be an array of strings'); }
+    if (typeof name !== 'string') { throw new TypeError('Name must be a string'); }
+    if (typeof length !== 'number') { throw new TypeError('Length must be a number'); }
+    if (!Array.isArray(students) || !students.every((item) => typeof item === 'string')) { throw new TypeError('Students must be an array of strings'); }
+
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  get name() {
-    return this._name;
-  }
+  get name() { return this._name; }
+
+  get length() { return this._length; }
+
+  get students() { return this._students; }
 
   set name(newValue) {
-    if (!this.validateString(newValue)) { throw new TypeError('Name must be a string'); }
+    if (typeof newValue !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = newValue;
   }
 
-  get length() {
-    return this._length;
-  }
-
   set length(newValue) {
-    if (!this.validateNumber(newValue)) { throw new TypeError('Length must be a number'); }
+    if (typeof newValue !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = newValue;
   }
 
-  get students() {
-    return this._students;
-  }
-
   set students(newValue) {
-    if (!this.validateStudents(newValue)) { throw new TypeError('Students must be an array of strings'); }
+    if (!Array.isArray(newValue) || !newValue.every((item) => typeof item === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = newValue;
-  }
-
-  validateString(value) {
-    return typeof value === 'string';
-  }
-
-  validateNumber(value) {
-    return typeof value === 'number';
-  }
-
-  validateStudents(value) {
-    return Array.isArray(value) && value.every((item) => typeof item === 'string');
   }
 }
