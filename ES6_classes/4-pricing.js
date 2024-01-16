b158fd417cd6a412ed388676a44a4927a6f7b079
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-16 14:16:04 +0100

    Push task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,20 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() { return this._amount; }
+
+  set amount(newValue) { this._amount = newValue; }
+
+  get currency() { return this._currency; }
+
+  set currency(newValue) { this._currency = newValue; }
+
+  displayFullPrice() { return `${this._amount} (${this._currency.displayFullCurrency()})`; }
+
+  convertPrice(amount, conversionRate) { return amount * conversionRate; }
+}
