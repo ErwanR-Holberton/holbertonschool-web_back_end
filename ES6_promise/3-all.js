18a5e355f08e8cffbd9838b9b499640f1b590408
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-24 14:55:34 +0100

    Push task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function handleProfileSignup() {
+    try {
+      const [{ body }, { firstName, lastName }] = await Promise.all([uploadPhoto(), createUser()]);
+      console.log(`${body} ${firstName} ${lastName}`);
+    } catch (error) {
+      console.error('Signup system offline', error);
+    }
+  }
