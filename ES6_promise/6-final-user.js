b7653b90a692d06d6aae9641cee77495ba9ab54c
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-26 10:21:41 +0100

    Changed line 5

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const result = await Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 
   const arrayResult = [];
   for (const prom of result) {
