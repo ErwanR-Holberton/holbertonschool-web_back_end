39298511565d565ee30952c02601649c63942b08
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-26 18:43:51 +0100

    Use settled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.value,
