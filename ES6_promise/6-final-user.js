33618d7e7ea1de349c900282c25f009e900bd28d
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-26 10:47:49 +0100

    Try new solution

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,20 +2,28 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-
-  const arrayResult = [];
-  for (const prom of result) {
-    const promResult = {
-      status: prom.status,
-      value: prom.status === 'fulfilled' && prom.value,
-    };
-
-    if (prom.status === 'rejected') {
-      promResult.value = prom.reason;
-    }
-
-    arrayResult.push(promResult);
+  let status;
+  let value;
+  let userData;
+  let photoData;
+  const resultArray = [];
+  try {
+    userData = await signUpUser(firstName, lastName);
+    status = 'fulfilled';
+    value = userData;
+  } catch (error) {
+    status = 'rejected';
+    value = error;
   }
-  return arrayResult;
+  resultArray.push({ status, value });
+  try {
+    photoData = await uploadPhoto(fileName);
+    status = 'fulfilled';
+    value = photoData;
+  } catch (error) {
+    status = 'rejected';
+    value = error;
+  }
+  resultArray.push({ status, value });
+  return resultArray;
 }
