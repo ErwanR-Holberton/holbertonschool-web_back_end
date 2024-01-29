f26ef7b7af063e5635350bc23cbd09e4e1f7c65b
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-29 17:59:06 +0100

    Added error

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) { throw new Error('Cannot process'); }
   return map.forEach((quantity, item) => {
     if (quantity === 1) {
       map.set(item, 100);
