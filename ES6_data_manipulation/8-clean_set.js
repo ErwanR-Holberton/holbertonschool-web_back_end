4b2e36cb63f32fca7027891e3d919adc90261ec0
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-29 17:40:14 +0100

    Push task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') { return ''; }
+  const filteredArray = [...set].filter((element) => element.startsWith(startString))
+    .map((element) => element.slice(startString.length));
+  const resultString = filteredArray.join('-');
+  return resultString;
+}
