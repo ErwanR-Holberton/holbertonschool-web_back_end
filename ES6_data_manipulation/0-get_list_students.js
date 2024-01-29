90ef723f05c5b209c14d399cc933bccf56f1afc1
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-29 12:32:15 +0100

    use map

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,13 @@
 export default function getListStudents() {
-  return [
+  const studentsData = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return studentsData.map((student) => ({
+    id: student.id,
+    firstName: student.firstName,
+    location: student.location,
+  }));
 }
