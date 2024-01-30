b8df87620bef3db71826aa4dc84e62c028f62bef
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-30 10:50:08 +0100

    Changed condition

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') { return ''; }
+  if (!startString) { return ''; }
   const filteredArray = [...set].filter((element) => element.startsWith(startString))
     .map((element) => element.slice(startString.length));
   const resultString = filteredArray.join('-');
