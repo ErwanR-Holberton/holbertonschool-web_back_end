9fdb72ec00f2768c67124f96f5538f6fb369eb02
ErwanR-Holberton <6218@holbertonstudents.com>
2024-01-30 16:09:38 +0100

    Push all

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const redHeader = document.querySelector('#red_header');
     redHeader.addEventListener('click', function() {
       const header = document.querySelector('header');
-      header.style.color = '#FF0000';
+      header.classList.add('red');
     });
   });
---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addItemElement = document.querySelector('#add_item');
+    const myListElement = document.querySelector('.my_list');
+    addItemElement.addEventListener('click', function() {
+      const newListItem = document.createElement('li');
+      newListItem.textContent = 'Item';
+      myListElement.appendChild(newListItem);
+    });
+  });
