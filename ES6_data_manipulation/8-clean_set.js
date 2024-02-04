ac03604eae34592c6510ef232c8f4a4a9248aa08
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-04 15:07:12 +0100

    Try with for each

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,12 @@
-export default function cleanSet(set, startString) {
+export default function cleanSet (set, startString) {
   if (!startString || (!typeof startString === 'string')) { return ''; }
-  const filteredArray = [...set].filter((element) => element.startsWith(startString))
-    .map((element) => element.slice(startString.length));
-  const resultString = filteredArray.join('-');
+  let resultString = '';
+
+  set.forEach(element => {
+    if (element.startsWith(startString)) {
+      if (resultString !== '') { resultString += '-'; }
+      resultString += element.slice(startString.lenght);
+    }
+  });
   return resultString;
 }
