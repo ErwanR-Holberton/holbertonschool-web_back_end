182ba505f34946db1b51fca029548f3ef2dcc0f8
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-04 15:35:32 +0100

    rollback

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,7 @@
-export default function cleanSet (set, startString) {
+export default function cleanSet(set, startString) {
   if (!startString || (!typeof startString === 'string')) { return ''; }
-  let resultString = '';
-
-  set.forEach(element => {
-    if (element && element.startsWith(startString)) {
-      if (resultString !== '') { resultString += '-'; }
-      resultString += element.slice(startString.length);
-    }
-  });
+  const filteredArray = [...set].filter((element) => element.startsWith(startString))
+    .map((element) => element.slice(startString.length));
+  const resultString = filteredArray.join('-');
   return resultString;
 }
